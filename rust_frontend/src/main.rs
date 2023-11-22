509d7296017dc999f6ab9d20f7deed79efe81e98
crunchyfrog <javaveryhot@protonmail.ch>
2023-11-22 13:12:29 +0100

    tar bort databasen från frontend

--- rust_frontend/src/main.rs
+++ rust_frontend/src/main.rs
@@ -1,29 +1,27 @@
-#[macro_use] extern crate rocket;
-use rocket::{fs::TempFile, form::{Form, validate}, response::{self, Responder, Response}, http::Status};
-use rocket_dyn_templates::{Template, context};
-use rocket_sync_db_pools::{database, diesel};
-
-#[database("postgres")]
-struct DbConn(diesel::PgConnection);
+#[macro_use]
+extern crate rocket;
+use rocket::{
+    form::{validate, Form},
+    fs::TempFile,
+    http::Status,
+    response::{self, Responder, Response},
+};
+use rocket_dyn_templates::{context, Template};
 
 #[derive(FromForm)]
 struct UploadForm<'a> {
     token: String,
-    file: TempFile<'a>
+    file: TempFile<'a>,
 }
 
 #[post("/upload-file", data = "<form>")]
-fn index(form: Form<UploadForm>, mut conn: DbConn) -> Status {
-
+fn index(form: Form<UploadForm>) -> Status {
     Status::BadRequest
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![
-            index
-        ])
+        .mount("/", routes![index])
         .attach(Template::fairing())
-        .attach(DbConn::fairing())
 }
