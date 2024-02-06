dbee8f0ec98331a6cd65e30b5e7caac5ed07c73b
SapraZ <sapraz@protonmail.com>
2024-02-06 13:57:58 +0100

    final

--- rust_backend/src/tests/mod.rs
+++ rust_backend/src/tests/mod.rs
@@ -9,30 +9,42 @@ use serde_json::json;
 use serde_json::Value;
 use tower_cookies::CookieManagerLayer;
 
-fn get_server() -> TestServer {
-    let app = Router::new()
-        .route("/register", post(register_account))
-        .route("/login", post(login_route))
-        .layer(CookieManagerLayer::new());
-    let config = axum_test::TestServerConfig::builder()
-        .default_content_type("application/json")
-        .build();
-
-    match axum_test::TestServer::new_with_config(app, config) {
-        Ok(o) => o,
-        Err(e) => {
-            println!("Failed to create test server: {}", e);
-            panic!("Failed to create test server");
-        }
-    }
-}
-
 #[cfg(test)]
 mod api_tests {
     use diesel::r2d2::R2D2Connection;
     use hyper::server::conn;
 
     use super::*;
+
+    fn get_server() -> TestServer {
+        let app = Router::new()
+            .route("/register", post(register_account))
+            .route("/login", post(login_route))
+            .layer(CookieManagerLayer::new());
+        let config = axum_test::TestServerConfig::builder()
+            .default_content_type("application/json")
+            .build();
+
+        match axum_test::TestServer::new_with_config(app, config) {
+            Ok(o) => o,
+            Err(e) => {
+                println!("Failed to create test server: {}", e);
+                panic!("Failed to create test server");
+            }
+        }
+    }
+
+    async fn perform_login(server: &TestServer, username: &str, password: &str) -> Value {
+        server
+            .post("/login")
+            .json(&json!({
+                "username": username,
+                "password": password,
+            }))
+            .await
+            .json::<Value>()
+    }
+
     #[tokio::test]
     async fn test_db_connection() {
         let mut conn = establish_connection();
